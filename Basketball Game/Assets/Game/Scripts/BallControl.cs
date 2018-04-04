259c6ec4377bae7c4f571f825619747612638305
pingak9 <khanhuitse05@gmail.com>
2018-04-04 18:18:07 +0700

    -Init

---
+++ Basketball Game/Assets/Game/Scripts/BallControl.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class BallControl : MonoBehaviour
+{
+    Rigidbody rigid;
+    public float timeLive = 6;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        rigid.isKinematic = true;
+    }
+
+    // Throw ball
+    void OnThrow()
+    {
+        float _distance = Vector3.Distance(posEnd, posBegin);
+        _distance = 1.5f + _distance / 400;
+        GameControl.Instance.txtDistance.text = "Distance: " + _distance.ToString();
+        GameControl.Instance.OnThown();
+        rigid.velocity = new Vector3(0, 1, 1) * _distance;
+
+        rigid.isKinematic = false;
+        transform.parent = null;
+        Invoke("DestroyBall", timeLive);
+    }
+    void DestroyBall()
+    {
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (isTouch)
+        {
+#if UNITY_EDITOR
+            if (Input.GetMouseButton(0))
+            {
+                posEnd = Input.mousePosition;
+                GameControl.Instance.txtEnd.text = "Pos End: " + posEnd.ToString();
+            }
+#else
+            if (Input.touchCount > 0)
+            {
+                posEnd = Input.touches[0].position;
+                GameControl.Instance.txtEnd.text = "Pos End: " + posEnd.ToString();
+            }
+#endif
+        }
+    }
+    #region Swipe
+    bool isTouch;
+    Vector3 posBegin;
+    Vector3 posEnd;
+    void OnMouseDown()
+    {
+        if (isTouch == false)
+        {
+            isTouch = true;
+            posBegin = GetPosTouch();
+            GameControl.Instance.txtBegin.text = "Pos Begin: " + posBegin.ToString();
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if (isTouch == true)
+        {
+            isTouch = false;
+            OnThrow();
+        }
+    }
+    Vector3 GetPosTouch()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            return pos;
+        }
+#else
+        if (Input.touchCount > 0)
+        {
+            Vector3 pos = Input.touches[0].position;
+            return pos;
+        }
+#endif
+        return Vector3.zero;
+    }
+    #endregion
+
+}
---
+++ Basketball Game/Assets/Game/Scripts/GameControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl Instance { get; private set; }
+    public GameObject pfBall;
+    public float timeSpawn = 5;
+    public Text txtBegin;
+    public Text txtEnd;
+    public Text txtDistance;
+    void Start () {
+        Instance = this;
+        SpawnBall();
+    }
+	
+	void SpawnBall()
+    {
+        GameObject _obj = Instantiate(pfBall, transform);
+        _obj.SetActive(true);
+	}
+    public void OnThown()
+    {
+        Invoke("SpawnBall", timeSpawn);
+    }
+}
